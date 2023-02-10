55a34f93abbaf11e2a01cb9a638d1367d1561a43
GGDANYGG <daniel.guzman.1111@gmail.com>
2023-02-09 19:37:20 -0600

    Introduccion a la web 6 y 7

---
+++ Intro-web-7/Js/DomJS.js
@@ -0,0 +1,4 @@
+function evento (){
+    document.write("Hola esta es una introduccion al JS con funciones y DOM")
+    alert("Ya fuimos al pipirin")
+}
---
+++ Intro-web-7/Js/Js01/Tiposdedatosyvariables.js
@@ -0,0 +1,62 @@
+var nombre= "Daniel";
+var edad=30;
+var tengomascotas=true;
+var tengrotrabajo=false;
+var tengosueldo; //undefinided
+
+let nombre= "Daniel";
+let edad=30;
+let tengomascotas=true;
+let tengrotrabajo=false;
+let tengosueldo; //undefinided
+
+
+//Ejercicio 1
+function horas(){
+    let horasTrabajadas = parseInt(prompt("Cuantas horas?"));
+    let coste = parseInt(prompt("Coste por hora"));
+    document.write("Total = "+(horasTrabajadas*coste));
+}
+
+function operacion(a,b,c,d){
+    let suma = a+b;
+    let mul = d*c;
+    let res = suma/mul;
+    return res  
+}
+console.log("la operacion es : "+operacion(3,2,2,5))
+
+//Ejercicio 2
+function imc(){
+    let peso = parseInt(prompt("Cual es tu peso en (lb)?"));
+    let estatura = parseInt(prompt("Cual es tu estatura (cm)?"));
+    peso = peso*0.453592;
+    estatura = estatura/100;
+    let imc = peso / (estatura**2);
+    if (imc<16) {
+        console.log("Criterio de ingreso");
+    }
+    else if(imc>=16 && imc<=16.9){
+        console.log("Infrapeso");
+    }
+    else if(imc>=17 && imc<=18.4){
+        console.log("Bajo peso");
+    }
+    else if(imc>=18.5 && imc<=24.9){
+        console.log("Peso Normal");
+    }
+    else if(imc>=25 && imc<=29.9){
+        console.log("Sobre peso");
+    }
+    else if(imc>=30 && imc<=34.9){
+        console.log("Obesidad premorbida");
+    }
+    else if(imc>=35 && imc<=45){
+        console.log("Obesidad morbida");
+    }
+    else if(imc>45){
+        console.log("Obesidad hipermorbida");
+    }
+    console.log("tu peso es :"+peso)
+    console.log("tu imc es de :"+imc)
+}
