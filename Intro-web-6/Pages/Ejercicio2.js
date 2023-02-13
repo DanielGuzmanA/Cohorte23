4a14545d0eff3a0a45aebf31bfca9e12b601b96e
GGDANYGG <daniel.guzman.1111@gmail.com>
2023-02-13 00:50:04 -0600

    Tarea

---
+++ Intro-web-6/Pages/Ejercicio2.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    alert ("Te iras a la izquierda")
+   }
+   function myFunction2(){
+    alert ("Este es el sitio web de tibia")
+   }
---
+++ Intro-web-7/Tarea/Tarea.js
@@ -0,0 +1,5 @@
+function myFunction(){
+ var name=prompt("Bienvenido, Cual es tu nombre?", "Nombre");
+ document.getElementById("Nombre").innerHTML = "Hola " + name;
+}
+
