0d51ccce13d717c2d8ec10f79d04b4a8cda3ffff
GGDANYGG <daniel.guzman.1111@gmail.com>
2023-03-06 17:40:12 -0600

    Java09

---
+++ Java/Java09/oop/src/testers/TestFecha.java
@@ -0,0 +1,14 @@
+package testers;
+
+import oop.Fecha;
+
+public class TestFecha {
+	public static void main(String[] args) {
+		Fecha f = new Fecha(6,3,22);
+		f.setDia(15);
+		f.setMes(3);
+		f.setYear(22);
+		System.out.println(f.diaMesYear());
+		System.out.println(f.toString());
+	}
+}
