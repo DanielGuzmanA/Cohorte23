850384dd56ad1e8f382104cf450c615f32055710
GGDANYGG <daniel.guzman.1111@gmail.com>
2023-03-22 17:21:29 -0600

    Java-web07

---
+++ Java-web07/Java-web-sts/demo00/src/test/java/com/cohorte23/generetion/Demo00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cohorte23.generetion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
